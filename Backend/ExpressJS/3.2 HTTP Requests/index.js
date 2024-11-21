2eb6c16a326744fa0bb2b7efc6e36349b6e8d2a5
Himanshu Verma <himanshuv309@gmail.com>
2024-11-22 00:25:33 +0530

    nodemon and res

--- Backend/ExpressJS/3.2 HTTP Requests/index.js
+++ Backend/ExpressJS/3.2 HTTP Requests/index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(req.rawHeaders)
+  //console.log(req.rawHeaders)
+  res.send("<h1>Hello Motto!</h1>")
 })
 
 app.listen(port, () => {
