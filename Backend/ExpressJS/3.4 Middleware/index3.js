c01af41698dffabda2177f2b0489a78f010807ae
Himanshu Verma <himanshuv309@gmail.com>
2024-11-28 16:45:03 +0530

    custom middleware

--- Backend/ExpressJS/3.4 Middleware/index3.js
+++ Backend/ExpressJS/3.4 Middleware/index3.js
@@ -3,10 +3,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+function buzzer(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  console.log("Request Host: ", req.hostname);
+  console.log("Request IP: ", req.ip);
+  next();
+}
+
+app.use(buzzer);
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello Buzzer");
 });
 
 app.listen(port, () => {
