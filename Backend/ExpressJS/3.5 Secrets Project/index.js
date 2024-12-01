712e97667a9245e8339f8f733204e4f4ebe58a50
Himanshu Verma <himanshuv309@gmail.com>
2024-12-01 18:03:57 +0530

    project

--- Backend/ExpressJS/3.5 Secrets Project/index.js
+++ Backend/ExpressJS/3.5 Secrets Project/index.js
@@ -2,3 +2,27 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: ture }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+});
+
+function secret(req, res, next){
+    console.log(req.body);
+    res.sendFile(__dirname + "/public/secret.html")
+    next();
+}
+ 
+app.use(secret);
+
