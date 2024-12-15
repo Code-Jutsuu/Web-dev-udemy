1485a9668b4058e67de557d4034c88afbf3e036a
Himanshu Verma <himanshuv309@gmail.com>
2024-12-15 18:32:33 +0530

    secret project

--- Backend/ExpressJS/3.5 Secrets Project/index.js
+++ Backend/ExpressJS/3.5 Secrets Project/index.js
@@ -2,27 +2,42 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: ture }));
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-function secret(req, res, next){
-    console.log(req.body);
-    res.sendFile(__dirname + "/public/secret.html")
-    next();
-}
- 
-app.use(secret);
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
